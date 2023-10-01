fa6c9a5699a592c1a35571f970f4abb90e7f3ff1
Arunraj-Logaraj <arunraj14898@gmail.com>
2023-10-02 01:15:12 +0530

    six

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -37,6 +37,18 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!initialized) {
+      setInitialized(true);
+    }
+  }, [initialized]);
+
+  if (!initialized) {
+    return <div></div>;
+  }
+
   return (
     <Canvas
       frameloop="demand"
